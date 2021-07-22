bc783dfa5980179548dbdafb54654ebbf5875c87
Thiago <tnfredes@globo.com>
2021-07-21 21:47:38 -0300

    atualizado

--- dashboard/jenkins/blueprint.py
+++ dashboard/jenkins/blueprint.py
@@ -1,3 +1,4 @@
+from os import name
 import flask as f
 import jenkins
 
@@ -10,3 +11,7 @@ def index():
     job_list = [client.get_job_info(j["name"])for j in job_list]
     return f.render_template("jenkins/index.html", jobs=job_list)
 
+@bp.route("/<nome>/editar")
+def edit_job(nome):
+    config = client.get_job_config(name)
+    return f.render_template("jenkins/edit")
