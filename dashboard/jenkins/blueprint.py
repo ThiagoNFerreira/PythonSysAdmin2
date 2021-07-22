ec81c41f712fc73cf058ca9fde51aec96b1998f7
Thiago <tnfredes@globo.com>
2021-07-21 21:06:40 -0300

    Mais um

---
+++ dashboard/jenkins/blueprint.py
@@ -0,0 +1,12 @@
+import flask as f
+import jenkins
+
+bp = f.Blueprint("jenkins", __name__, url_prefix="/jenkins")
+client = jenkins.Jenkins("http://localhost:8080", username="admin", password="loca1020")
+
+@bp.route("")
+def index():
+    job_list = client.get_jobs()
+    job_list = [client.get_job_info(j["name"])for j in job_list]
+    return f.render_template("jenkins/index.html", jobs=job_list)
+
