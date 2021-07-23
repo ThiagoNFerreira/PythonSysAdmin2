cf34b092dd73a9420cc563a5f6fb55f5650f05b1
Thiago <tnfredes@globo.com>
2021-07-22 21:43:33 -0300

    Com SSH

---
+++ ssh/main.py
@@ -0,0 +1,26 @@
+import paramiko.client
+import paramiko
+
+client = paramiko.client.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(
+    paramiko.AutoAddPolicy()
+)
+
+client.connect(
+    "localhost",
+    username="developer",
+    password="4linux")
+
+(stdin, stdout, stderr) = client.exec_command("ls")
+
+status = stdout.channel.recv_exit_status()
+if status == 0:
+    stdout.flush()
+    print(stdout.read().decode())
+
+else:
+    stderr.flush()
+    print(stderr.read().decode())
+
+client.close()
